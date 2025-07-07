7a6b9181f6d422e024f71a17b98fca37ae270961
Lightpearl26 <38223361+Lightpearl26@users.noreply.github.com>
2025-07-07 21:59:12 +0200

    Create project description

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Shift project
-2-player platformer
+
+---
+
+**Project name** : Shift project  
+**Authors**      : Lafiteau Franck | Castaing Guillaume  
+**version**      : a0.1  
+
+---
+
+This project is a fanmade game where two players are playing a platformer game. One of the player is
+controlling the main character trying to pass the game and the other plays his sidekick that is here
+to help him during his journey
+
+---
+
+(c) Copyrights *Lafiteau Franck* | *Castaing Guillaume*
---
+++ game.py
@@ -0,0 +1,28 @@
+#!venv/Scripts/python.exe
+#-*- coding: utf-8 -*-
+
+"""
+SHIFT PROJECT
+____________________________________________________________________________________________________
+project name : Shift Project
+authors      : Lafiteau Franck | Castaing Guillaume
+version      : a0.1
+____________________________________________________________________________________________________
+This project is a fanmade game where two players are playing a platformer game. One of the player is
+controlling the main character trying to pass the game and the other plays his sidekick that is here
+to help him during his journey
+____________________________________________________________________________________________________
+copyrights: (c) Franck Lafiteau (code)
+            (c) Guillaume Castaing (main idea, level design, graphics)
+"""
+
+# Create main function of the script
+def main() -> None:
+    """
+    Main function of the script
+    this function is called when launching the script
+    """
+    print("Hello world !")
+
+if __name__ == "__main__":
+    main()
